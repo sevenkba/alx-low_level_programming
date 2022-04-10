3309d13830e47788b12bd2be5da061873e82a176
kidus biniyam <kidusbicsn3@gmail.com>
2022-04-10 16:04:33 -0700

    My seventh commit message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *My hello, printf task
+ *Return: return 1 after program executes
+ */
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
+}
