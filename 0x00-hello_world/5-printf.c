ae8bd8caaa4e1b73d3a5f4f77efe9951bd318eda
kidus biniyam <kidusbicsn3@gmail.com>
2022-04-10 16:14:19 -0700

    edited commit of seventh message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
- *main -entry point
- *My hello, printf task
- *Return: return 1 after program executes
- */
 int main(void)
 {
-printf("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammer, but the outcome is a piece of art,");
 return (0);
 }
